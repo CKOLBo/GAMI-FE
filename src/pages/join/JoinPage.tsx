21337b86e5e7ca9c677fe2293b5f3891f32a3085
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-18 09:19:41 +0900

    feat: join 페이지 퍼블리싱 옮기기

---
+++ src/pages/join/JoinPage.tsx
@@ -0,0 +1,35 @@
+export default function Join() {
+  return (
+    <body>
+      <div className="login">
+        <img src="/logo.svg" alt="GAMI 로고" className="logo" />
+        <p className="join">
+          이미 회원이신가요? <a href="/login/login.html">로그인</a>
+        </p>
+        <form className="input">
+          <input type="text" placeholder="이름" required />
+          <input type="text" placeholder="기수" required />
+          <div className="sex">
+            <button className="male" type="button">
+              남자
+            </button>
+            <button className="female" type="button">
+              여자
+            </button>
+          </div>
+          <select className="major" placeholder="전공" required>
+            <option value="" disabled selected>
+              전공
+            </option>
+            <option value="fe">FrontEnd</option>
+            <option value="be">BackEnd</option>
+            <option value="ios">iOS</option>
+          </select>
+          <button className="next" type="submit">
+            다음으로
+          </button>
+        </form>
+      </div>
+    </body>
+  );
+}
