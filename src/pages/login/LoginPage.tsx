5e95327fbb16ff0426ea738f5122fd79dbbedc14
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-18 11:38:57 +0900

    refactor: LoginPage에 React Router 적용
    - 컴포넌트 함수명 추가 (anonymous → LoginPage)
    - a 태그를 Link 컴포넌트로 변경
    - /signup으로 연결되도록 경로 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,10 +1,12 @@
-export default function () {
+import { Link } from 'react-router-dom';
+
+export default function LoginPage() {
   return (
     <body>
       <div className="로그인">
         <img src="/logo.svg" alt="GAMI 로고" className="logo" />
         <p className="회원가입">
-          GAMI가 처음이라면? <a href="/join/join.html">회원가입하기</a>
+          GAMI가 처음이라면? <Link to="/signup">회원가입하기</Link>
         </p>
         <form className="입력">
           <input type="text" placeholder="이메일" required />
@@ -12,9 +14,9 @@ export default function () {
           <button type="submit">로그인</button>
         </form>
         <div className="한국">
-          <a href="login.html" className="비번찾기">
+          <Link to="/login" className="비번찾기">
             비밀번호 찾기
-          </a>
+          </Link>
         </div>
       </div>
     </body>
