685d89072a52dccede0d74e3893d7828d74b7834
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-27 23:45:03 +0900

    style: border color 변경

--- src/pages/signup/signup1/SignupJoinPage.tsx
+++ src/pages/signup/signup1/SignupJoinPage.tsx
@@ -26,18 +26,18 @@ export default function JoinPage() {
             type="text"
             placeholder="이름"
             required
-            className="p-[16px] border-[1px] border-[solid] border-[#ddd] rounded-[6px] text-[14px] outline-none"
+            className="p-[16px] border-[1px] border-[solid] border-[#B7BCC8] rounded-[6px] text-[14px] outline-none"
           />
           <div className="flex gap-[2%]">
             <button
               type="button"
-              className="bg-[white] border-[1px] border-[solid] border-[#ddd] rounded-[6px] text-[14px] text-[#858486] font-[500] p-[16px] w-[49%] cursor-pointer transition-all duration-[300ms] hover:border-[#91bbff] hover:text-[#91bbff] outline-none"
+              className="bg-[white] border-[1px] border-[solid] border-[#B7BCC8] rounded-[6px] text-[14px] text-[#858486] font-[500] p-[16px] w-[49%] cursor-pointer transition-all duration-[300ms] hover:border-[#91bbff] hover:text-[#91bbff] outline-none"
             >
               남자
             </button>
             <button
               type="button"
-              className="bg-[white] border-[1px] border-[solid] border-[#ddd] rounded-[6px] text-[14px] text-[#858486] font-[500] p-[16px] w-[49%] cursor-pointer transition-all duration-[300ms] hover:border-[#91bbff] hover:text-[#91bbff] outline-none"
+              className="bg-[white] border-[1px] border-[solid] border-[#B7BCC8] rounded-[6px] text-[14px] text-[#858486] font-[500] p-[16px] w-[49%] cursor-pointer transition-all duration-[300ms] hover:border-[#91bbff] hover:text-[#91bbff] outline-none"
             >
               여자
             </button>
@@ -45,7 +45,7 @@ export default function JoinPage() {
           <select
             required
             defaultValue=""
-            className="bg-[white] border-[1px] border-[solid] border-[#ddd] rounded-[6px] text-[14px] text-[#858486] font-[500] p-[16px] w-[100%] cursor-pointer appearance-none outline-none"
+            className="bg-[white] border-[1px] border-[solid] border-[#B7BCC8] rounded-[6px] text-[14px] text-[#858486] font-[500] p-[16px] w-[100%] cursor-pointer appearance-none outline-none"
           >
             <option value="" disabled>
               기수
