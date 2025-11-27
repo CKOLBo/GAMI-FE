2cd5988e4dec91cf8674445596e03e2e8e74037b
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-27 18:02:12 +0900

    chore: SignupEmailPage로 파일명 변경

---
+++ src/pages/signup/signup3/SignupEmailPage.tsx
@@ -0,0 +1,42 @@
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '@/assets/Logo/Logo';
+import '@/pages/signup/signup3/SignupEmailPage.css';
+
+export default function EmailPage() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate('/signup/password');
+  };
+
+  return (
+    <div className="email-page">
+      <div className="email-container">
+        <Logo />
+        <p className="login-link">
+          이미 회원이신가요? <Link to="/login">로그인하기</Link>
+        </p>
+        <form className="email-form" onSubmit={handleSubmit}>
+          <div className="auth-input-group">
+            <input type="email" placeholder="이메일" required />
+            <button type="button" className="auth-button">
+              인증하기
+            </button>
+          </div>
+
+          <div className="auth-check-group">
+            <input type="text" placeholder="인증번호" required />
+            <button type="button" className="check-button">
+              확인
+            </button>
+          </div>
+
+          <button type="submit" className="submit-button">
+            가입완료
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
