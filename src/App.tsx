3c7490d6f4916c037147549d0f666c17dec732a9
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-28 14:45:49 +0900

    fix: email 경로 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/login/LoginPage';
 import Password from '@/pages/signup/signup2/SignupPasswordPage';
 import JoinPage from '@/pages/signup/signup1/SignupJoinPage';
-import EmailPage from '@/pages/signup/signup3/SignupEmailPage';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup/join" element={<JoinPage />} />
-        <Route path="/signup/email" element={<EmailPage />} />
         <Route path="/signup/password" element={<Password />} />
       </Routes>
     </BrowserRouter>
