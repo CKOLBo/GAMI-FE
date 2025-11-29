236b8263e88ac63d7248f2536b44f01833285d67
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-30 02:48:14 +0900

    style: CategoryButton hover 색상 변경

--- src/assets/components/CategoryButton.tsx
+++ src/assets/components/CategoryButton.tsx
@@ -15,7 +15,7 @@ export default function CategoryButton({
       className={`px-[12px] py-[12px] rounded-full text-[14px] font-[500] transition-all duration-200 ${
         isSelected
           ? 'bg-[#BFA9FF] border-[#BFA9FF] border-[2px] text-[#ffffff]'
-          : 'bg-[#ffffff] text-[#6D6F79] border-[2px] border-[#B7BCC8] hover:bg-[#AA8EFF]'
+          : 'bg-[#ffffff] text-[#6D6F79] border-[2px] border-[#B7BCC8] hover:bg-[#BFA9FF]'
       }`}
     >
       {label}
