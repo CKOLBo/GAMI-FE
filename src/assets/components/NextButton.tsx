4754e1fce9b9b937b52b959da9610008f9bc9b11
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-29 13:18:58 +0900

    feat: NextButton 컴포넌트 생성

---
+++ src/assets/components/NextButton.tsx
@@ -0,0 +1,10 @@
+export default function NextButton() {
+  return (
+    <button
+      type="submit"
+      className="p-[16px] w-[320px] bg-[#BFA9FF] text-[white] text-[16px] border-none rounded-[6px] cursor-pointer transition-all duration-[300ms] mt-[10px] font-[700] hover:bg-[#AA8EFF] outline-none"
+    >
+      다음으로
+    </button>
+  );
+}
