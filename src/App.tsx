21f7c8bddd8f020f0b4fc67a942d58c1b7969537
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-30 00:07:59 +0900

    feat: App.tsx에 Signup2 루트 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/login/LoginPage';
 import Step3 from '@/pages/signup/signup3/SignupStep3';
+import Step2 from '@/pages/signup/signup2/SignupStep2';
 import Step1 from '@/pages/signup/signup1/SignupStep1';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup1" element={<Step1 />} />
+        <Route path="/signup2" element={<Step2 />} />
         <Route path="/signup3" element={<Step3 />} />
       </Routes>
     </BrowserRouter>
