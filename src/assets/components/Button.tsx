f6a8df3072e4009c02a87608e0b4c0bab71def20
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-07 21:29:25 +0900

    feat: button 컴포넌트 ui 제작

---
+++ src/assets/components/Button.tsx
@@ -0,0 +1,12 @@
+export default function Button() {
+  return (
+    <div>
+      <button
+        type="button"
+        className="rounded-[10px] bg-main-1 w-38 h-16 text-white font-bold text-2xl"
+      >
+        글 쓰기
+      </button>
+    </div>
+  );
+}
