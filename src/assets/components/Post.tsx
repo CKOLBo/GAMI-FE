6198e9660401d299161d795f4a16444548ac1c30
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-08 23:07:37 +0900

    refactor: svg 폴더 삭제

---
+++ src/assets/components/Post.tsx
@@ -0,0 +1,15 @@
+export default function Post() {
+  return (
+    <div>
+      <div>
+        <p>제목이 들어갈 곳</p>
+        <p>내용이 들어갈 곳</p>
+      </div>
+      <div>
+        <p>익명</p>
+        <p>1시간 전</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
