ec7ea705a34e119ea2cdde9ca14e25200440e885
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-09 23:45:11 +0900

    refactor: 파일명 변경

--- src/assets/svg/Heart.tsx
+++ src/assets/svg/Heart.tsx
@@ -1,8 +1,4 @@
-interface HartProps {
-  isSelect: boolean;
-}
-
-export default function Hart({ isSelect }: HartProps) {
+export default function Heart() {
   return (
     <svg
       width="35"
@@ -12,14 +8,13 @@ export default function Hart({ isSelect }: HartProps) {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
         d="M17.5 4.95579C14.3011 1.27877 8.9556 0.142417 4.94752 3.50946C0.93943 6.8765 0.375145 12.506 3.52272 16.4882C6.13972 19.7991 14.0597 26.7822 16.6554 29.0424C16.9457 29.2953 17.0909 29.4217 17.2604 29.4713C17.4081 29.5146 17.5699 29.5146 17.7178 29.4713C17.8872 29.4217 18.0323 29.2953 18.3228 29.0424C20.9185 26.7822 28.8383 19.7991 31.4554 16.4882C34.603 12.506 34.1075 6.84108 30.0305 3.50946C25.9535 0.177837 20.6989 1.27877 17.5 4.95579Z"
-        fill={isSelect ? '#FF6B6B' : 'none'}
         stroke="#333D48"
-        strokeWidth="3"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
