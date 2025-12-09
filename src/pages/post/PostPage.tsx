5417aaabe60758cb78e8c347e782a65e83a38b42
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-09 09:16:07 +0900

    feat: PostPage 퍼블리싱 1차 완료(sidebar 컴포넌트 추후 추가 예정)

--- src/pages/post/PostPage.tsx
+++ src/pages/post/PostPage.tsx
@@ -2,25 +2,74 @@ import InputSearch from '@/assets/components/InputSearch';
 import Button from '@/assets/components/Button';
 import Post from '@/assets/components/Post';
 export default function PostPage() {
+  const posts = [
+    {
+      id: 1,
+      title: '제목이 들어갈 곳',
+      content: '내용이 들어갈 곳',
+      author: '익명',
+      timeAgo: '14건 전',
+      likeCount: 3,
+      commentCount: 0,
+    },
+    {
+      id: 2,
+      title: '제목이 들어갈 곳',
+      content: '내용이 들어갈 곳',
+      author: '익명',
+      timeAgo: '14건 전',
+      likeCount: 3,
+      commentCount: 0,
+    },
+    {
+      id: 3,
+      title: '제목이 들어갈 곳',
+      content: '내용이 들어갈 곳',
+      author: '익명',
+      timeAgo: '14건 전',
+      likeCount: 3,
+      commentCount: 0,
+    },
+    {
+      id: 4,
+      title: '제목이 들어갈 곳',
+      content: '내용이 들어갈 곳',
+      author: '익명',
+      timeAgo: '14건 전',
+      likeCount: 3,
+      commentCount: 0,
+    },
+  ];
+
   return (
-    <>
-      <div className="flex flex-row my-25">
+    <div>
+      <div className="flex items-center my-22">
         <div>
           <p className="text-[40px] w-46 h-12 font-bold mx-25">익명 게시판</p>
         </div>
-        <div>
-          <InputSearch />
-        </div>
-        <div className="ml-114 flex">
-          <Button text="글 쓰기" to="" />
-        </div>
-        <div className="ml-14">
-          <Button text="내가 쓴 글" to="" />
+        <div className="flex flex-row gap-114">
+          <div>
+            <InputSearch />
+          </div>
+          <div className="flex gap-14">
+            <Button text="글 쓰기" to="/post/write" />
+            <Button text="내가 쓴 글" to="/post/my" />
+          </div>
         </div>
       </div>
       <div>
-        <Post />
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            content={post.content}
+            author={post.author}
+            timeAgo={post.timeAgo}
+            likeCount={post.likeCount}
+            commentCount={post.commentCount}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
