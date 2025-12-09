01f7bb8dbd2ca850efea2726d69c4b2635862708
koreahghg <koreahghg@gmail.com>
2025-12-09 13:54:16 +0900

    feat: App 컴포넌트에 MainPage 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SigninPage from '@/pages/signin/SigninPage';
 import Signup from '@/pages/signup/SignupPage';
+import Main from '@/pages/main/MainPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Navigate to="/signin" replace />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
