6309f571bbf03ac0cb8998e3aea26c067fa3fdeb
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-09 09:39:05 +0900

    refactor: Comment svg camelCase로 변경

--- src/assets/svg/Comment.tsx
+++ src/assets/svg/Comment.tsx
@@ -10,9 +10,9 @@ export default function Comment() {
       <path
         d="M1.5 15.5C1.5 23.232 7.76796 29.5 15.5 29.5C18.6677 29.5 29.4989 29.5 29.4989 29.5C29.4989 29.5 27.0735 23.6762 28.0441 21.7235C28.976 19.8487 29.5 17.7356 29.5 15.5C29.5 7.76802 23.232 1.5 15.5 1.5C7.76796 1.5 1.5 7.76802 1.5 15.5Z"
         stroke="#333D48"
-        stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
