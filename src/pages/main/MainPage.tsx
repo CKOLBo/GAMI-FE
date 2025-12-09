5c857fc928f904d43be7f92920d943af1a4ad900
koreahghg <koreahghg@gmail.com>
2025-12-09 13:52:07 +0900

    feat: MainPage 컴포넌트에 Sidebar 추가

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,10 @@
+import Sidebar from '@/assets/components/Sidebar';
+
+export default function MainPage() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1"></div>
+    </div>
+  );
+}
