3f3ba511096b5d619c9d79e26e0803c2aa4ba6b5
koreahghg <koreahghg@gmail.com>
2025-12-10 16:58:59 +0900

    fix: CommentIcon SVG 속성을 React 규칙에 맞게 수정

--- src/assets/svg/main/CommentIcon.tsx
+++ src/assets/svg/main/CommentIcon.tsx
@@ -10,9 +10,9 @@ export default function CommentIcon() {
       <path
         d="M1 7C1 10.3137 3.68627 13 7 13C8.3576 13 12.9995 13 12.9995 13C12.9995 13 11.9601 10.5041 12.3761 9.6672C12.7754 8.86373 13 7.95813 13 7C13 3.68629 10.3137 1 7 1C3.68627 1 1 3.68629 1 7Z"
         stroke="#333D48"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
