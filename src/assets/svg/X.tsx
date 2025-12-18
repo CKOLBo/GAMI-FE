6898c8342ecb76ec8e1cb8055d6af36e68b284ec
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-18 11:26:58 +0900

    X 아이콘 컴포넌트 추가

---
+++ src/assets/svg/X.tsx
@@ -0,0 +1,7 @@
+export default function X() {
+    return(
+<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.75 6.04175L7.25 20.5417M7.25 6.04175L21.75 20.5417" stroke="#6D6F79" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+    );
+}
