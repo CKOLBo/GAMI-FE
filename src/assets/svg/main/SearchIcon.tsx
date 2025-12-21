8a2a0ecbe07e8f8715bdc3052923a83324c38ae7
koreahghg <koreahghg@gmail.com>
2025-12-21 11:36:30 +0900

    feat: SearchIcon 컴포넌트에 color 프로퍼티 추가

--- src/assets/svg/main/SearchIcon.tsx
+++ src/assets/svg/main/SearchIcon.tsx
@@ -1,8 +1,12 @@
 interface SearchIconProps {
   className?: string;
+  color?: string;
 }
 
-export default function SearchIcon({ className }: SearchIconProps) {
+export default function SearchIcon({
+  className,
+  color = '#333D48',
+}: SearchIconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,7 +18,7 @@ export default function SearchIcon({ className }: SearchIconProps) {
     >
       <path
         d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-        stroke="currentColor"
+        stroke={color}
         strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
