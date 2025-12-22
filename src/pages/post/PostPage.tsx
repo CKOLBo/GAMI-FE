0a77239573d43ddc48f07aeb448269e317dba895
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-22 14:14:39 +0900

    feat: sidebar 추가

--- src/pages/post/PostPage.tsx
+++ src/pages/post/PostPage.tsx
@@ -6,6 +6,7 @@ import PostModal from '@/assets/components/modal/ReportModal';
 import { useState, useEffect } from 'react';
 import Report from '@/assets/svg/post/Report';
 import axios from 'axios';
+import Sidebar from '@/assets/components/Sidebar';
 
 interface PostType {
   id: number;
@@ -42,42 +43,45 @@ export default function PostPage() {
   };
 
   return (
-    <div className="w-full">
-      <div className="max-w-[1500px] mx-auto px-4 lg:px-6">
-        <PostHead>
-          <div className="flex flex-wrap gap-3 sm:gap-4 lg:gap-6">
-            <Button text="글 쓰기" to="/post-write" />
-            <Button text="내가 쓴 글" to="/my-post" />
-          </div>
-        </PostHead>
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full flex-1">
+        <div className="max-w-[1500px] px-4 ml-80 xl-ml:55 lg:px-6">
+          <PostHead>
+            <div className="flex flex-wrap gap-3 sm:gap-4 lg:gap-6">
+              <Button text="글 쓰기" to="/post-write" />
+              <Button text="내가 쓴 글" to="/my-post" />
+            </div>
+          </PostHead>
 
-        <div className="border-t-2 border-gray-2">
-          {postData.map((post) => (
-            <Post
-              key={post.id}
-              title={post.title}
-              content={post.content}
-              author={post.author}
-              likeCount={post.likeCount}
-              commentCount={post.commentCount}
-              timeAgo={post.timeAgo}
-              onPostClick={() => navigate('/post-content')}
-              actions={[
-                {
-                  icon: <Report />,
-                  onClick: () => handleReportClick(),
-                },
-              ]}
-            />
-          ))}
+          <div className="border-t-2 border-gray-2">
+            {postData.map((post) => (
+              <Post
+                key={post.id}
+                title={post.title}
+                content={post.content}
+                author={post.author}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                timeAgo={post.timeAgo}
+                onPostClick={() => navigate('/post-content')}
+                actions={[
+                  {
+                    icon: <Report />,
+                    onClick: () => handleReportClick(),
+                  },
+                ]}
+              />
+            ))}
+          </div>
         </div>
+        {isModalOpen && (
+          <PostModal
+            onClose={() => setIsModalOpen(false)}
+            onReport={handleReport}
+          />
+        )}
       </div>
-      {isModalOpen && (
-        <PostModal
-          onClose={() => setIsModalOpen(false)}
-          onReport={handleReport}
-        />
-      )}
     </div>
   );
 }
