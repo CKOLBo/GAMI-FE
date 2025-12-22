15c4038b6afdf147244d27e1593739d8e200b3ad
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-22 20:03:41 +0900

    feat: ai 요약 부분 퍼블리싱

--- src/pages/post/PostContent.tsx
+++ src/pages/post/PostContent.tsx
@@ -5,6 +5,8 @@ import Report from '@/assets/svg/post/Report';
 import PostModal from '@/assets/components/modal/ReportModal';
 import Button from '@/assets/components/Button/Button';
 import Sidebar from '@/assets/components/Sidebar';
+import Gemini from '@/assets/svg/post/Gemini.png';
+import Arrow from '@/assets/svg/Arrow';
 
 export default function PostContent() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -29,6 +31,26 @@ export default function PostContent() {
           <div className="mb-34 mt-18">
             <div className="text-xl leading-relaxed font-bold text-gray-3 whitespace-pre-wrap">
               내용이 들어갈 곳 내용이 들어갈 곳 내용이 들어갈 곳
+            </div>
+          </div>
+
+          <div className="flex gap-12 mb-34">
+            <div className="border rounded-full p-4 w-18 h-18 border-gray-2">
+              <img src={Gemini} alt="Gemini" width="44" height="44" />
+            </div>
+
+            <div className="rounded-[20px] shadow-GAMI pt-6 pl-8 w-[464px] h-[140px]">
+              <p className="text-gray-3 font-bold text-xl pb-8">
+                AI를 활용 해 게시글을 요약해보세요!
+              </p>
+              <div className="flex gap-1 cursor-pointer">
+                <h2 className="text-gray-1 font-bold text-2xl">
+                  AI로 요약하기
+                </h2>
+                <div className="flex items-center">
+                  <Arrow className="w-7 h-7" />
+                </div>
+              </div>
             </div>
           </div>
 
