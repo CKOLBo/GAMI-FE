e6070e85a7d746f19555170a762ac02740e3b5e2
koreahghg <koreahghg@gmail.com>
2025-12-22 15:15:07 +0900

    style: SearchIcon 컴포넌트의 크기 및 클래스 수정

--- src/assets/svg/main/SearchIcon.tsx
+++ src/assets/svg/main/SearchIcon.tsx
@@ -10,11 +10,9 @@ export default function SearchIcon({
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
-      className={className}
+      className={className || "w-6 h-6"}
     >
       <path
         d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
