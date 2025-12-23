8e32d6c8f63eaa30094be9be12370da7f5063ab2
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-23 10:26:34 +0900

    refactor: PostHead 컴포넌트에서 children 제거 및 버튼 위치 조정

--- src/assets/components/post/PostHead.tsx
+++ src/assets/components/post/PostHead.tsx
@@ -1,17 +1,16 @@
+import Button from '../Button/Button';
 import InputSearch from '../Input/InputSearch';
 
-interface PostHeadProps {
-  children: React.ReactNode;
-}
-
-export default function PostHead({ children }: PostHeadProps) {
+export default function PostHead() {
   return (
     <div className="flex flex-col lg:flex-row items-start lg:items-center my-4 sm:my-6 lg:my-16 gap-3 lg:gap-20">
       <div className="flex flex-col sm:flex-row w-full flex-wrap sm:items-center gap-3 sm:gap-4 justify-between">
         <div className="w-full sm:flex-1 max-w-100">
           <InputSearch />
         </div>
-        {children}
+        <div className="flex pl-[410px]">
+          <Button text="글 쓰기" to="/post-write" />
+        </div>
       </div>
     </div>
   );
