8b023db7d7c1a70e735387ac1cc7a38fc2b0e941
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-23 09:47:54 +0900

    fix: 게시글 내용 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Signin from '@/pages/signin/SigninPage';
 import Signup from '@/pages/signup/SignupPage';
 import Main from '@/pages/main/MainPage';
 import Post from '@/pages/post/PostPage';
-import PostContent from './pages/post/PostContent';
+import PostContent from './pages/post/PostId';
 import PostWrite from './pages/post/PostWrite';
 import MyPost from './pages/post/MyPost';
 import Password from '@/pages/password/PasswordPage';
@@ -36,7 +36,7 @@ function App() {
           <Route element={<ProtectedLayout />}>
             <Route path="/main" element={<Main />} />
             <Route path="/post" element={<Post />} />
-            <Route path="/post-content" element={<PostContent />} />
+            <Route path="/post/id" element={<PostContent />} />
             <Route path="/post-write" element={<PostWrite />} />
             <Route path="/my-post" element={<MyPost />} />
             <Route path="/password" element={<Password />} />
